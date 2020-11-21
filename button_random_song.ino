cd3d130d1bcac95abfd8cce8bdfc5d70657a8f97
Ro <50117686+programandoconro@users.noreply.github.com>
2020-11-21 19:51:13 +0900

    Create button_random_song.ino
    This code is used to play a random song in a Raspberry 3B+ when a button is pushed using an Arduino Micro board. Push again for a new song. Reset the Arduino to stop the music. Enjoy !!

---
+++ button_random_song.ino
@@ -0,0 +1,55 @@
+/*
+  Keyboard Message to play a random song in a Raspberry (or any computer) 
+
+  For the Arduino Leonardo and Micro.
+
+  Requirements: 
+  
+  - Arduino Leonardo or Micro, Raspberry pi (or any computer). Speakers or earphones.  
+  - Install vlc (``sudo apt install vlc``).
+  - Clone this repo in your ``/home/pi`` directory (modify if needed).
+  
+  Sends a message to vlc to play a random song when a button is pressed.
+
+  The circuit:
+  - Button attached from pin 4 to +5V
+  - 10 kilohm resistor attached from pin 4 to ground
+  - Connect to a Raspberry Pi
+  - Connect the speakers
+  - Push the button for a random song
+  - Push again for a new random song
+  - Push reset button to stop
+
+  created 21 Sept 2020
+  by programandoconro
+
+*/
+
+#include "Keyboard.h"
+boolean off = true;
+const int buttonPin = 4;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  Keyboard.begin();
+}
+
+void loop() {
+  if (off == true) {
+    delay(1000);
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.write('c');
+    Keyboard.releaseAll();
+    off = false;
+  }
+
+  if (buttonState == true) {
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.write('c');
+    Keyboard.releaseAll();
+    delay(500);
+    Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack && vlc . -Z");
+    delay(1000);
+  }
+
+}
