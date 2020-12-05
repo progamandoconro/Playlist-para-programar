aa9043b5f229587b102226c8a9f1dfa88fa497a5
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-06 00:59:42 +0900

    Update random-music.sh
    Random music in the phone using Termux and play-audio.

--- random-music.sh
+++ random-music.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/bash
+
+# This script can be used in Termux (Android) to play random music in the Phone. 
+# Need to install termux, clone this repo and install play-audio.
+
 cd /data/data/com.termux/files/home/Playlist-para-programar/Soundtrack/ && play-audio $(ls | sort -R)
