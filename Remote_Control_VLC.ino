1385832fc89f359e195854e210c331804f701b69
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-12 16:19:45 +0900

    Create Remote_Control_VLC.ino
    Use an Arduino remote control for your Raspberry Pi with VLC music player.

---
+++ Remote_Control_VLC.ino
@@ -0,0 +1,125 @@
+//Arduino Remote control for Raspberry pi with VLC music player. 
+//Clone this repo and modify this script to look music in your folder. 
+
+//Programandoconro 2020 (modified from Elegoo 2016)
+
+#include "IRremote.h"
+#include "Keyboard.h"
+
+int receiver = 11; // Signal Pin of IR receiver to Arduino Digital Pin 11
+boolean PLAY = false;
+boolean POWER = false;
+
+/*-----( Declare objects )-----*/
+IRrecv irrecv(receiver);     // create instance of 'irrecv'
+decode_results results;      // create instance of 'decode_results'
+
+/*-----( Function )-----*/
+void translateIR() // takes action based on IR code received
+
+// describing Remote IR codes
+
+{
+
+  switch (results.value)
+
+  {
+
+    case 0xFFA25D: Serial.println("POWER");
+
+      if (POWER == false) {
+        POWER = true;
+      }
+      else {
+        POWER = false;
+      }
+
+      if (POWER == true) {
+        Keyboard.press(KEY_LEFT_CTRL); Keyboard.write('c'); Keyboard.releaseAll(); delay(500);
+        Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack && vlc . -Z"); delay(500);
+
+      }
+
+      if (POWER == false) {
+        Keyboard.println("quit");
+      }
+
+      break;
+
+
+    case 0xFF02FD: Serial.println("PAUSE");
+
+      if (PLAY == false) {
+        PLAY = true;
+      }
+      else {
+        PLAY = false;
+      }
+
+      if (PLAY == true) {
+     
+        Keyboard.println("play");
+
+      }
+
+      if (PLAY == false) {
+        Keyboard.println("pause");
+
+      }
+      break;
+
+    case 0xFFE21D: Serial.println("FUNC/STOP"); Keyboard.println("quit"); break;
+    case 0xFF629D: Serial.println("VOL+"); Keyboard.println("volup"); break;
+    case 0xFF22DD: Serial.println("FAST BACK"); Keyboard.println("prev"); break;
+    case 0xFFC23D: Serial.println("FAST FORWARD"); 
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c'); 
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack && vlc . -Z"); 
+      break;
+    case 0xFFE01F: Serial.println("DOWN");    break;
+    case 0xFFA857: Serial.println("VOL-");   Keyboard.println("voldown"); break;
+    case 0xFF906F: Serial.println("UP");    break;
+    case 0xFF9867: Serial.println("EQ");    break;
+    case 0xFFB04F: Serial.println("ST/REPT"); Keyboard.println("prev"); break;
+    case 0xFF6897: Serial.println("0");    break;
+    case 0xFF30CF: Serial.println("1");    break;
+    case 0xFF18E7: Serial.println("2");    break;
+    case 0xFF7A85: Serial.println("3");    break;
+    case 0xFF10EF: Serial.println("4");    break;
+    case 0xFF38C7: Serial.println("5");    break;
+    case 0xFF5AA5: Serial.println("6");    break;
+    case 0xFF42BD: Serial.println("7");    break;
+    case 0xFF4AB5: Serial.println("8");    break;
+    case 0xFF52AD: Serial.println("9");    break;
+    case 0xFFFFFFFF: Serial.println(" REPEAT"); break;
+
+    default:
+      Serial.println(" other button   ");
+
+  }// End Case
+
+  delay(500); // Do not get immediate repeat
+
+
+} //END translateIR
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+  Serial.begin(9600);
+  Serial.println("IR Receiver Button Decode");
+  irrecv.enableIRIn(); // Start the receiver
+
+}/*--(end setup )---*/
+
+
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  if (irrecv.decode(&results)) // have we received an IR signal?
+
+  {
+    translateIR();
+    irrecv.resume(); // receive the next value
+  }
+}/* --(end main loop )-- */
+
