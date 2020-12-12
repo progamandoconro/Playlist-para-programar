cbf1c8c4862fa9ddaf0828dd60dd12c5d686e54d
Ro <50117686+programandoconro@users.noreply.github.com>
2020-12-12 19:01:54 +0900

    Update remote_control_VLC.ino

--- remote_control_VLC.ino
+++ remote_control_VLC.ino
@@ -3,6 +3,9 @@
 
 //Programandoconro 2020 (modified from Elegoo 2016)
 
+//www.elegoo.com
+//2016.12.9
+
 #include "IRremote.h"
 #include "Keyboard.h"
 
@@ -57,7 +60,7 @@ void translateIR() // takes action based on IR code received
       }
 
       if (PLAY == true) {
-     
+
         Keyboard.println("play");
 
       }
@@ -71,28 +74,96 @@ void translateIR() // takes action based on IR code received
     case 0xFFE21D: Serial.println("FUNC/STOP"); Keyboard.println("quit"); break;
     case 0xFF629D: Serial.println("VOL+"); Keyboard.println("volup"); break;
     case 0xFF22DD: Serial.println("FAST BACK"); Keyboard.println("prev"); break;
-    case 0xFFC23D: Serial.println("FAST FORWARD"); 
+    case 0xFFC23D: Serial.println("FAST FORWARD");
       Keyboard.press(KEY_LEFT_CTRL);
-      Keyboard.write('c'); 
+      Keyboard.write('c');
       Keyboard.releaseAll();
       delay(500);
-      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack && vlc . -Z"); 
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack && vlc . -Z");
       break;
-    case 0xFFE01F: Serial.println("DOWN");    break;
+    case 0xFFE01F: Serial.println("DOWN");
+      break;
     case 0xFFA857: Serial.println("VOL-");   Keyboard.println("voldown"); break;
-    case 0xFF906F: Serial.println("UP");    break;
+    case 0xFF906F: Serial.println("UP");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Ray_Charles && vlc . -Z");
+      break;
     case 0xFF9867: Serial.println("EQ");    break;
     case 0xFFB04F: Serial.println("ST/REPT"); Keyboard.println("prev"); break;
-    case 0xFF6897: Serial.println("0");    break;
-    case 0xFF30CF: Serial.println("1");    break;
-    case 0xFF18E7: Serial.println("2");    break;
-    case 0xFF7A85: Serial.println("3");    break;
-    case 0xFF10EF: Serial.println("4");    break;
-    case 0xFF38C7: Serial.println("5");    break;
-    case 0xFF5AA5: Serial.println("6");    break;
-    case 0xFF42BD: Serial.println("7");    break;
-    case 0xFF4AB5: Serial.println("8");    break;
-    case 0xFF52AD: Serial.println("9");    break;
+    case 0xFF6897: Serial.println("0");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Sakura && vlc . -Z");
+
+      break;
+    case 0xFF30CF: Serial.println("1");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Nihongo && vlc . -Z");
+      break;
+    case 0xFF18E7: Serial.println("2");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Beatles && vlc . -Z");
+      break;
+    case 0xFF7A85: Serial.println("3");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Elton_John && vlc . -Z");
+      break;
+    case 0xFF10EF: Serial.println("4");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Kate_Bush && vlc . -Z");
+      break;
+    case 0xFF38C7: Serial.println("5");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Rock && vlc . -Z");
+      break;
+    case 0xFF5AA5: Serial.println("6");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Venezuela && vlc . -Z");
+      break;
+    case 0xFF42BD: Serial.println("7");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Bob_Marley && vlc . -Z");
+      break;
+    case 0xFF4AB5: Serial.println("8");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Espana && vlc . -Z");
+      break;
+    case 0xFF52AD: Serial.println("9");
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.write('c');
+      Keyboard.releaseAll();
+      delay(500);
+      Keyboard.println("cd /home/pi/Playlist-para-programar/Soundtrack/Match_Box_20 && vlc . -Z");
+      break;
     case 0xFFFFFFFF: Serial.println(" REPEAT"); break;
 
     default:
@@ -122,4 +193,7 @@ void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
     irrecv.resume(); // receive the next value
   }
 }/* --(end main loop )-- */
+
+
+
 
